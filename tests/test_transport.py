091a629c61c21c4aa61e9224291235930cb36804
data-guin <lawsonbh@gmail.com>
2019-05-02 11:31:34 -0400

    updated

--- tests/test_transport.py
+++ tests/test_transport.py
@@ -5,18 +5,18 @@ from pycontest.transport import transport
 
 
 # # first process is transport, and we can easily write a simple test
-# def test_transport_1():
-#     loc = 3.
-#     vel = 1.
-#     dt = .5
-#     assert transport(loc, vel, dt) == 3.5
+def test_transport_1():
+     loc = 3.
+     vel = 1.
+     dt = .5
+     assert transport(loc, vel, dt) == 3.5
 #
 # # but we also want to check if it works for arrays with 2d
-# def test_transport_2():
-#     loc = np.array([[1, 2], [11, 12]])
-#     vel = np. array([[1, 1], [-1, -1]])
-#     dt = 1
-#     assert (transport(loc, vel, dt) == np.array([[2, 3], [10, 11]])).all()
+def test_transport_2():
+     loc = np.array([[1, 2], [11, 12]])
+     vel = np. array([[1, 1], [-1, -1]])
+     dt = 1
+     assert (transport(loc, vel, dt) == np.array([[2, 3], [10, 11]])).all()
 #
 #
 # # let's try one more, with dt that is float
